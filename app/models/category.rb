7659a40ec791e407ed08ba692ab16ba3da63bdca
donovanrichardson <donovanspqa@gmail.com>
2020-08-19 19:34:42 -0400

    in the set_recipe method (recipes_controller.rb:43) the find_by_id returns the falsy nil if none is found, while find throws an error (i think) so i prefer that method

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
+
+  #validate the presence in the model
+  validates_presence_of :title, :created_by
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :category
+
+  # validation
+  validates_presence_of :name
 end
